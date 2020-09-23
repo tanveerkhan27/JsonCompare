7cabbb0386fb116ebb218e62428eaef56c4661d6
Tanveer Khan <tanveer@Tanveers-MacBook-Pro.local>
2020-09-24 01:36:30 +0530

    added logging mechanism for comparison verification

--- Comparer/JValueComparer.cs
+++ Comparer/JValueComparer.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
+using NUnit.Framework;
 
 /** Author - Tanveer Khan; email - tanveer.khan@outlook.com **/
 
@@ -12,22 +13,32 @@ namespace JsonCompare.Comparer
 
     class JValueComparer : JTokenComparerBase<JValue>
     {
-        protected override int CompareDerived(JValue x, JValue y)
+        protected override int CompareDerived(JValue sourceValue, JValue targetValue)
         {
-            if (!(x.Type == JTokenType.Null))
+            
+            if (!(sourceValue.Type == JTokenType.Null))
             {
-                if (y.Value.ToString().ToLower() == "$jsonignore")
+                if (targetValue.Value.ToString().ToLower() == "$jsonignore")
                 {
-                    var xparentProperty = ((JProperty)x.Parent);
-                    var yparentProperty = ((JProperty)y.Parent);
+                    var xparentProperty = ((JProperty)sourceValue.Parent);
+                    var yparentProperty = ((JProperty)targetValue.Parent);    
                     if (xparentProperty.Name == yparentProperty.Name)
                         Console.WriteLine("{0}:{1} value has been ignored", xparentProperty.Name, xparentProperty.Value);
                         return 0;
                 }
             }
-
-            return Comparer<JToken>.Default.Compare(x, y); // JValue implements IComparable<JValue>
-
+            
+            int comp = Comparer<JToken>.Default.Compare(sourceValue, targetValue);
+            
+            if(comp!=0)
+            {
+                var xparentProperty = ((JProperty)sourceValue.Parent);
+                var yparentProperty = ((JProperty)targetValue.Parent);
+                Assert.Fail("{0}:{1} value has not matched", xparentProperty.Name, xparentProperty.Value);
+                return 1;
+            }
+            else
+                return 0;
         }
     }
 }
